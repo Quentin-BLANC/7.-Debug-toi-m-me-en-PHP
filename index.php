9c22cd62082c2e60d21cd8677127428ace455691
Quentin-BLANC <quentinblanc587@gmail.com>
2020-10-03 19:54:24 +0200

    7. Debug toi même en PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+$characters = [
+    "Negan" => [
+        "city" => "The Sanctuary",
+        "weapon" => "Lucille"
+    ],
+    "Daryl" => [
+        "city" => "Alexandria",
+        "weapon" => "crossbow"
+    ],
+    "Ezekiel" => [
+        "city" => "The Kingdom",
+        "weapon" => "Shiva"
+    ],
+    "Quentin" => [
+        "city" => "Tours",
+        "weapon" => "Câline"
+    ]
+];
+
+var_dump($characters);
